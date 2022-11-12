fe43e142ba1ca6c3714b358d6844f31a8eeccc2a
kidstonb <113933033+kidstonb@users.noreply.github.com>
2022-11-12 13:58:49 -0500

    Second Draft

--- PyPoll.py
+++ PyPoll.py
@@ -1,16 +1,40 @@
 # In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
 
 # You will be given a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: "Voter ID", "County", and "Candidate". Your task is to create a Python script that analyzes the votes and calculates each of the following:
+import os 
+import csv
 
-# * The total number of votes cast
+# variable initialization 
+election = {}
 
-# * A complete list of candidates who received votes
+csv_path = os.path.join("PyPoll","Resources","election_data.csv")
 
-# * The percentage of votes each candidate won
+with open(csv_path,'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
 
-# * The total number of votes each candidate won
+    csv_header = next(csv_reader)
+
+    # Adds candidate to dictionary if not present, otherwise increments the vote for that candidate
+    for row in csv_reader:
+        if row[2] not in election:
+            election[row[2]] = 1
+        else:
+            election[row[2]] += 1
+    
+# Totals the votes from the dictionary
+votes_total = sum(election.values())
+
+# Print of results per candidate- total votes, percentages
+print('--------------------\nElection Results\n--------------------')
+print(f'Total Votes: {votes_total}\n--------------------')
+for candidate in election:
+    print(f'{candidate}: {round(100*election[candidate]/votes_total,3)} % ({election[candidate]})')
 
 # * The winner of the election based on popular vote.
+winner = 0
+for candidate in election:
+    if 
+print('--------------------\nWinner: {}\n--------------------')
 
 # Your analysis should look similar to the following:
 
--- Pybank.py
+++ Pybank.py
@@ -1,29 +1,68 @@
 # Your task is to create a Python script that analyzes the records to calculate each of the following:
-import csv
+# import pandas - try not to use for this assignment
+import csv 
 import os 
 
-# intialize values 
-total_months = 0 
-P_L = 0 
-Increase = 0 
-Decrease = 0 
+# variables initialization
+# total_months = 0 
+# P_L = 0 
+greatest_profit = 0 
+lowest_profit = 0 
+profit_list = []
+change_list = []
 
-filepath = os.path.join("PyBank","Resources","budget_data.csv")
+csv_path = os.path.join("PyBank","Resources","budget_data.csv")
 
-with open(filepath,'r') as csvfile
-    csvreader = csv.reader(csvfile,delimiter=',')
-    #header = next(csvreader)
+#with open(csv_pathpath,'r') as csvfile
+with open(csv_path,'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
 
+    csv_header = next(csv_reader)
+    # print(f"CSV Header: {csv_header}")
 
-# * The total number of months included in the dataset
+    for row in csv_reader:
+        # * The total number of months included in the dataset
+        total_months += 1
+        #print(type(int(row[1])))
 
-# * The net total amount of "Profit/Losses" over the entire period
+        # * The net total amount of "Profit/Losses" over the entire period
+    # Try to check if any inconsistencies in the dates?
+        P_L += int(row[1])
+        # Here I collect 
+        profit_list.append(int(row[1]))
+
+        # * The greatest increase in profits (date and amount) over the entire period
+        # * The greatest decrease in profits (date and amount) over the entire period
+        if int(row[1]) > greatest_profit:
+            greatest_profit = int(row[1])
+            greatest_date = row[0]
+        if int(row[1]) < lowest_profit:
+            lowest_profit = int(row[1])
+            lowest_date = row[0]
+
+# * The changes in "Profit/Losses" over the entire period, and then the average of those changes
+for i in range(len(profit_list)-1):
+    change_list.append(profit_list[i+1] - profit_list[i])
+
+average_change = sum(change_list) / len(change_list)
+
+print('----------------------------\nFinancial Analysis\n----------------------------')
+print(f'Total Months: {len(profit_list)}')      # or just len(profit_list) or total_months
+print(f'Total: ${sum(profit_list)}')                     # or sum(profit_list) or P_L
+print(f'Average Change: ${round(average_change,2)}')
+print(f'Greatest Increase in Profits: {greatest_date} (${greatest_profit})')
+print(f'Greatest Decrease in Profits: {lowest_date} (${lowest_profit})')
+print('----------------------------')
+
+# output_path = os.path.join("output", "Financial_Analysis.txt")
+# with open(output_path, 'w') as csvfile:
+
 
 # * The changes in "Profit/Losses" over the entire period, and then the average of those changes
 
-# * The greatest increase in profits (date and amount) over the entire period
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-# * The greatest decrease in profits (date and amount) over the entire period
+
 
 # Your analysis should look similar to the following:
 
@@ -37,4 +76,3 @@ with open(filepath,'r') as csvfile
 #   Greatest Decrease in Profits: Feb-14 ($-1825558)
 #   ```
 
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
